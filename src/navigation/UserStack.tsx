27dc3accb2db447051da7aa172aa260204ff16f1
bmccabe96 <brianmccabe96@gmail.com>
2022-09-04 13:02:40 -0400

    Added bottom tab navigator to UserStack

--- src/navigation/UserStack.tsx
+++ src/navigation/UserStack.tsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { RecipesProvider } from '../context/Recipes';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 import {
   RecipesScreen,
@@ -11,20 +14,57 @@ import {
 import { RootStackParamList } from '../../src/types';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
+const Tab = createBottomTabNavigator();
+
+const RecipeStack = () => {
+  return (
+    <Stack.Navigator initialRouteName="Recipes">
+      <Stack.Screen name="Recipes" component={RecipesScreen} />
+      <Stack.Screen
+        name="RecipeDetail"
+        component={RecipeDetailScreen}
+        options={{ title: 'Recipe Details' }}
+      />
+    </Stack.Navigator>
+  );
+};
 
 const UserStack = () => {
   return (
-    <NavigationContainer>
-      <AccountScreen />
-      {/* <Stack.Navigator initialRouteName="Recipes">
-        <Stack.Screen name="Recipes" component={RecipesScreen} />
-        <Stack.Screen
-          name="RecipeDetail"
-          component={RecipeDetailScreen}
-          options={{ title: 'Recipe Details' }}
-        />
-      </Stack.Navigator> */}
-    </NavigationContainer>
+    <RecipesProvider>
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName: string = '';
+              color = 'blue';
+
+              if (route.name === 'Food') {
+                iconName = focused
+                  ? 'fast-food'
+                  : 'fast-food-outline';
+              } else if (route.name === 'Account') {
+                iconName = focused ? 'settings' : 'settings-outline';
+              }
+
+              // You can return any component that you like here!
+              return (
+                <Ionicons name={iconName} size={size} color={color} />
+              );
+            },
+            tabBarActiveTintColor: 'blue',
+            tabBarInactiveTintColor: 'gray',
+          })}
+        >
+          <Tab.Screen
+            name="Food"
+            component={RecipeStack}
+            options={{ headerShown: false }}
+          />
+          <Tab.Screen name="Account" component={AccountScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </RecipesProvider>
   );
 };
 
