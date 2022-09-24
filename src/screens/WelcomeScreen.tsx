b1defcd175ad50183b534b5c98c30be4846771ea
bmccabe96 <brianmccabe96@gmail.com>
2022-09-24 18:23:49 -0400

    minor style tweak on background colors

--- src/screens/WelcomeScreen.tsx
+++ src/screens/WelcomeScreen.tsx
@@ -36,7 +36,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: 20,
-    backgroundColor: '#fff',
+    backgroundColor: '#f0f9fa',
     alignItems: 'center',
     justifyContent: 'center',
   },
